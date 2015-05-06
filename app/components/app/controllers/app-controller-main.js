fb945bae69c5db814eb548e85f77233d16f1ed8a
Rudi Starcevic <ooly.me@gmail.com>
2015-05-06 15:50:13 +0800

    Add inital code for icon font size depending on media query size

--- app/components/app/controllers/app-controller-main.js
+++ app/components/app/controllers/app-controller-main.js
@@ -1,11 +1,19 @@
 (function (module) {
   'use strict';
 
-  function MainCtrl(rsmConfig) {
+  function MainCtrl($mdMedia, rsmConfig) {
 
     this.greeting = 'Hello World';
 
     this.version = rsmConfig.version;
+
+    var pix;
+    ($mdMedia('gt-md')) ? pix = 12 : pix = 18;
+    console.log('pix', pix);
+
+    console.log('mdMedia', $mdMedia('sm'));
+    console.log('mdMedia', $mdMedia('gt-md'));
+    console.log('mdMedia', $mdMedia('max-width: 300px'));
 
   }
 
